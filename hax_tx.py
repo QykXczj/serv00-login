471a716994d7e42fe8697f7e4deb404151a842ac
QykXczj <116059420+QykXczj@users.noreply.github.com>
2024-09-15 05:06:09 +0800

    Update hax_tx.py

--- hax_tx.py
+++ hax_tx.py
@@ -1,15 +1,16 @@
 import requests
+import os
 
 # 从环境变量中获取 Telegram Bot Token 和 Chat ID
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 VX_BOT_KEY = os.getenv('VX_BOT_KEY')
 
-def send_message(self, message):
-    self.send_telegram_message(message)
-    self.send_VX_Bot_message(message)
+def send_message(message):
+    send_telegram_message(message)
+    send_VX_Bot_message(message)
 
-def send_telegram_message(self, message):
+def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {
         'chat_id': TELEGRAM_CHAT_ID,
@@ -25,7 +26,7 @@ def send_telegram_message(self, message):
     except Exception as e:
         print(f"发送消息到Telegram时出错: {e}")
 
-def send_VX_Bot_message(self, message):
+def send_VX_Bot_message(message):
     url = f"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={VX_BOT_KEY}"
     payload = {
         "msgtype": "text",
@@ -44,4 +45,3 @@ def send_VX_Bot_message(self, message):
         print(f"发送消息到VX_Bot时出错: {e}")
 
 send_message("hax该续了https://hax.co.id/vps-renew/")
-
